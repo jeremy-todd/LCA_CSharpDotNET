082148833a8b7e610ea616b7a8aed73d7e034378
jeremy-todd <jeremy.todd@tylertech.com>
2019-06-12 15:25:06 -0500

    Updates to make CarLot functional

--- CarLot/Program.cs
+++ CarLot/Program.cs
@@ -10,94 +10,121 @@ namespace CarLot
     {
         static void Main(string[] args)
         {
-            CarLot MallParking = new CarLot();
-            Truck F150 = new Truck()
-            {
-                BedSize = 5,
-                LicenseNumber = "G1ZBG",
-                Make = "Ford",
-                Model = "F-150",
-                Price = 40000
-            };
-            MallParking.AddCar(F150);
-            Car Elantra = new Car()
-            {
-                Doors = 4,
-                LicenseNumber = "HG63TV",
-                Make = "Hyundai",
-                Model = "Elantra",
-                Price = 24000,
-                Type = "Sedan"
-            };
-            MallParking.PrintInventory();
+            CarLot C1 = new CarLot("C-1");
+            CarLot R7 = new CarLot("R-7");
+            Vehicle F150 = new Truck("G1ZBG", "Ford", "F-150", 40000, 5);
+            Vehicle Elantra = new Car("HG63TV", "Hyundai", "Elantra", 24000, "Sedan", 4);
+            C1.AddCar(F150, Elantra);
+            Vehicle Tundra = new Truck("LBK-3399", "Toyota", "Tundra", 45000, 6);
+            Vehicle Corvette = new Car("NCM-5453", "Chevrolet", "Corvette", 65000, "Coupe", 2);
+            R7.AddCar(Tundra, Corvette);
+            C1.PrintInventory();
+            Console.WriteLine(" ");
+            R7.PrintInventory();
+            Console.Read();
         }
     }
 
     class CarLot
     {
-        public string Name { get; set; }
-        public List<Vehicle> ListCars { get; set; }
+         //fields
+         public string Name { get; private set; }
 
-        public CarLot()
+        //constructors
+        public CarLot (string name)
         {
-            
+             Name = name;
         }
 
-        public virtual void AddCar(Truck F150)
-        {
-            ListCars.Add(F150);
-        }
+       public List<Vehicle> ListCars = new List<Vehicle>();
 
-        public virtual void PrintInventory()
-        {
-            foreach (Vehicle car in ListCars)
-            {
-                Console.WriteLine(car);
-            }
-            Console.Read();
-        }
+       //methods
+       public virtual void AddCar(Vehicle F150, Vehicle Elantra)
+       {
+           ListCars.Add(F150);
+           ListCars.Add(Elantra);
+       }
+
+       public virtual void PrintInventory()
+       {
+           Console.WriteLine("Parking lot {0} has the following vehicles parked in it:",Name);
+           foreach (Vehicle vehicle in ListCars)
+           {
+               Console.WriteLine(vehicle.VehicleDesc());
+           }
+       }
     }
 
     abstract class Vehicle
     {
+        //fields
         public string LicenseNumber { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
         public int Price { get; set; }
 
-        public Vehicle()
+        //constructors
+        public Vehicle(string licenseNumber, string make, string model, int price)
         {
-
+            LicenseNumber = licenseNumber;
+            Make = make;
+            Model = model;
+            Price = price;
         }
 
-        public virtual void VehicleDesc()
+        //methods
+        public virtual string VehicleDesc()
         {
-            Console.WriteLine("License Number: {0}", LicenseNumber);
-            Console.WriteLine("Make: {0}", Make);
-            Console.WriteLine("Model: {0}", Model);
-            Console.WriteLine("Price: {0}", Price);
+            string VehicleDesc = Make + " " + Model + " with license plate " + LicenseNumber + " that costs $" + Price + ".";
+            return VehicleDesc;
         }
     }
 
     class Truck : Vehicle
     {
+        //fields
         public int BedSize { get; set; }
 
-        public Truck()
+        //constructors
+        public Truck(string licenseNumber, string make, string model, int price, int bedSize):base(licenseNumber, make, model, price)
         {
+            LicenseNumber = licenseNumber;
+            Make = make;
+            Model = model;
+            Price = price;
+            BedSize = bedSize;
+        }
 
+        //methods
+        public override string VehicleDesc()
+        {
+            string VehicleDesc = Make + " " + Model + " with license plate " + LicenseNumber + " that costs $" + Price + " and has a " + BedSize + "' bed.";
+            return VehicleDesc;
         }
     }
 
     class Car : Vehicle
     {
+        //fields
         public string Type { get; set; }
         public int Doors { get; set; }
 
-        public Car()
+        //constructors
+        public Car(string licenseNumber, string make, string model, int price, string type, int doors):base(licenseNumber, make, model, price)
         {
+            LicenseNumber = licenseNumber;
+            Make = make;
+            Model = model;
+            Price = price;
+            Type = type;
+            Doors = doors;
+        }
 
+        //methods
+        public override string VehicleDesc()
+        {
+            string VehicleDesc = Make + " " + Model + " is a " + Doors + " door " + Type + " with license plate " + LicenseNumber + " that costs $" + Price + ".";
+            return VehicleDesc;
         }
     }
-
 }
